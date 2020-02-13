1264b16b79aa10ffa68fd55100895fcc2b481780
@UyUong <ngouyoung@yahoo.com>
2020-02-13 18:10:06 +0700

    change footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2019-{{ \Carbon\Carbon::now()->format('Y') }} <a href="javascript:void(0)">HELLO TEAM</a>.</strong>
+    <strong>Copyright &copy; 2019-{{ \Carbon\Carbon::now()->format('Y') }} <a href="javascript:void(0)">VNAT-Software TEAM</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
         <b>Make by your 💖</b>
